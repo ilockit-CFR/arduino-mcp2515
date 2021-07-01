3be43eda572c19580fb00e2ee7efe45febd5b652
ilockit-CFR <39822173+ilockit-CFR@users.noreply.github.com>
2021-07-01 12:02:39 +0200

    Anpassung für Testmodus
    Clock-Frequenz mit 8 MHz initialisiert
    Pause zw. den Sendevorgängen auf 1 Sek. gesetzt.

--- examples/CAN_write/CAN_write.ino
+++ examples/CAN_write/CAN_write.ino
@@ -1,8 +1,11 @@
+#include <can.h>
 #include <SPI.h>
 #include <mcp2515.h>
 
+struct can_frame canMsg;
 struct can_frame canMsg1;
-struct can_frame canMsg2;
+unsigned char counter = 0;
+
 MCP2515 mcp2515(10);
 
 
@@ -17,33 +20,24 @@ void setup() {
   canMsg1.data[5] = 0x8E;
   canMsg1.data[6] = 0xBE;
   canMsg1.data[7] = 0x86;
-
-  canMsg2.can_id  = 0x036;
-  canMsg2.can_dlc = 8;
-  canMsg2.data[0] = 0x0E;
-  canMsg2.data[1] = 0x00;
-  canMsg2.data[2] = 0x00;
-  canMsg2.data[3] = 0x08;
-  canMsg2.data[4] = 0x01;
-  canMsg2.data[5] = 0x00;
-  canMsg2.data[6] = 0x00;
-  canMsg2.data[7] = 0xA0;
   
   while (!Serial);
   Serial.begin(115200);
   
   mcp2515.reset();
-  mcp2515.setBitrate(CAN_125KBPS);
+  mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);
   mcp2515.setNormalMode();
   
   Serial.println("Example: Write to CAN");
 }
 
 void loop() {
+
+//  counter++;
+//  canMsg1.data[7] = counter;
   mcp2515.sendMessage(&canMsg1);
-  mcp2515.sendMessage(&canMsg2);
 
   Serial.println("Messages sent");
-  
-  delay(100);
+
+  delay(1000);
 }
